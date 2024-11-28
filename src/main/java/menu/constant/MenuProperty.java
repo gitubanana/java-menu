5b9b03de4b530bf4230b3a8a3bee743b54c522a5
gitubanana <ptyolo666@gmail.com>
2024-11-28 17:09:55 +0900

    feat(MenuProperty): 메뉴 관련 상수 클래스

---
+++ src/main/java/menu/constant/MenuProperty.java
@@ -0,0 +1,14 @@
+package menu.constant;
+
+public class MenuProperty {
+    public static final int MIN_COACH_COUNT = 2;
+    public static final int MAX_COACH_COUNT = 5;
+    public static final int MIN_NAME_LENGTH = 2;
+    public static final int MAX_NAME_LENGTH = 4;
+    public static final int MAX_ALLERGIC_COUNT = 2;
+    public static final String NAME_DELIMITER = ",";
+    public static final String ALLERGIC_FOOD_DELIMITER = ",";
+    public static final int DAY_COUNT = 5;
+    public static final int MAX_SAME_CATEGORY_COUNT = 2;
+    public static final int MENU_INDEX = 0;
+}
